146e653fdd0a18ea7022355c445a9291dd2d809b
Trevor Cickovski <tcickovs@castalia.cs.fiu.edu>
2020-06-02 16:06:33 -0400

    Cluster number was off by one

--- TightnessStatisticsPlugin.py
+++ TightnessStatisticsPlugin.py
@@ -24,16 +24,27 @@ class TightnessStatisticsPlugin:
       for i in range(len(self.mean)):
          # Order: Mean within self, stddev within self, mean with others, stddev with others
          internalTuple = [None, None, None, None]
-         internalTuple[0] = float(self.mean[i][i])
-         internalTuple[1] = float(self.std[i][i])
+         if (self.mean[i][i] == ''):
+            internalTuple[0] = 0
+         else:
+            internalTuple[0] = float(self.mean[i][i])
+         if (self.std[i][i] == ''):  # No standard deviation if only 2 nodes in cluster
+            internalTuple[1] = 0
+         else:
+            internalTuple[1] = float(self.std[i][i])
          sumMean = 0
          sumStd = 0
          for j in range(0, i):
-            sumMean += float(self.mean[j][i])
-            sumStd += float(self.std[j][i])
+            if (self.mean[j][i] != ''):
+               sumMean += float(self.mean[j][i])
+            if (self.std[j][i] != ''):
+               sumStd += float(self.std[j][i])
+            #sumStd += float(self.std[j][i])
          for j in range(i+1, len(self.mean)):
-            sumMean += float(self.mean[i][j])
-            sumStd += float(self.std[i][j])
+            if (self.mean[i][j] != ''):
+               sumMean += float(self.mean[i][j])
+            if (self.std[i][j] != ''):
+               sumStd += float(self.std[i][j])
          sumMean /= (len(self.mean)-1)
          sumStd /= (len(self.mean)-1)
          internalTuple[2] = sumMean
@@ -48,7 +59,7 @@ class TightnessStatisticsPlugin:
          sumMeanOuter = 0
          sumStdOuter = 0
          for i in range(len(self.results)):
-            stats.write(str(i)+","+str(self.results[i][0])+","+str(self.results[i][1])+","+str(self.results[i][2])+","+str(self.results[i][3])+"\n")
+            stats.write(str(i+1)+","+str(self.results[i][0])+","+str(self.results[i][1])+","+str(self.results[i][2])+","+str(self.results[i][3])+"\n")
             sumMeanInner += self.results[i][0]
             sumStdInner += self.results[i][1]
             sumMeanOuter += self.results[i][2]
